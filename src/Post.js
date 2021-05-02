7c6fc5c6428b54a4f774b9b8bf067e9ba74bdf76
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-02 10:00:32 -0700

    2021

--- src/Post.js
+++ src/Post.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import './Post.css';
 
 function Post() {
   return (
     <div className="post">
+      <div className="post__header">
+      <Avatar 
+        className="post__avatar"
+        alt={username}
+        src="/static/images/avatar/1.jpg"
+      />
       <h3>Username</h3>
       {/* header -> avatar + username */}
 
       <img className="post__image" src="https://freecodecamp.org/news/content/images"></img>
       {/* image */}
 
-      <h4>Username: caption</h4>
+      <h4 className="post__text"><strong>two_t_elliott </strong>Man do I wonder sometimes</h4>
       {/* username + caption */}
+      </div>
     </div>
   )
 }
