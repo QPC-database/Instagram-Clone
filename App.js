8c6e9c8538edaf49efbd0d1f6c333664f56a762b
Elliott Thomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-04 11:37:21 -0700

    Add files via upload
    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Post from './Post';
+
+function App() {
+  return (
+    <div className="app">
+     
+    {/* Header - to contain instagram, search, icons, etc.*/} 
+    <div className="app__header">
+      <img
+        className="app__headerImage"
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt=""
+      />
+    </div>
+
+    <h1>Instagram Clone</h1>
+
+    <Post />
+    {/* Posts */}
+    {/* Posts */}
+    </div>
+  );
+}
+
+export default App;
